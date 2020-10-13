efb6dd8a6b3d99b96c51fc59cf88af1775d79237
NaJ <naj10000@live.com>
2020-10-12 22:31:25 -0400

    little stretch

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -47,6 +47,16 @@ const ColorList = ({ colors, updateColors }) => {
     .catch(err => console.log(err))
   };
 
+  const addColor = e => {
+    e.preventDefault();
+    axiosWithAuth()
+     .post('/api/colors', body)
+     .then(res => {
+       colorToEdit.id = Date.now()
+       updateColors(res.data)
+     })
+  }
+
   return (
     <div className="colors-wrap">
       <p>colors</p>
@@ -96,12 +106,41 @@ const ColorList = ({ colors, updateColors }) => {
           </label>
           <div className="button-row">
             <button type="submit">save</button>
+
             <button onClick={() => setEditing(false)}>cancel</button>
           </div>
         </form>
       )}
       <div className="spacer" />
-      {/* stretch - build another form here to add a color */}
+      <form onSubmit={addColor}>
+          <legend>add color</legend>
+          <label>
+            color name:
+            <input
+              onChange={e =>
+                setColorToEdit({ ...colorToEdit, color: e.target.value })
+              }
+              value={colorToEdit.color}
+            />
+          </label>
+          <label>
+            hex code:
+            <input
+              onChange={e =>
+                setColorToEdit({
+                  ...colorToEdit,
+                  code: { hex: e.target.value }
+                })
+              }
+              value={colorToEdit.code.hex}
+            />
+          </label>
+          <div className="button-row">
+            <button type="submit">addColor</button>
+           
+          </div>
+        </form>
+      
     </div>
   );
 };
